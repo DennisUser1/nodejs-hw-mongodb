b1eb507b29d7f1a95e6b16b9e77e23dd4570887c
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-11-18 17:05:48 +0200

    v.1.4. Added JSON.stringify transformations

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -4,17 +4,22 @@ import { getAllContactsService } from '../services/contacts.js';
 export const getAllContacts = async (req, res) => {
     try {
         const contacts = await getAllContactsService();
-        res.status(200).json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts
-        });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(
+            JSON.stringify({
+                status: 200,
+                message: "Successfully found contacts!",
+                data: contacts
+            }, null, 2)
+        );
     } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: "Error fetching contacts",
-            error: error.message
-        });
+        res.status(500).send(
+            JSON.stringify({
+                status: 500,
+                message: "Error fetching contacts",
+                error: error.message
+            }, null, 2)
+        );
     }
 };
 
@@ -26,16 +31,21 @@ export const getContactById = async (req, res) => {
         if (!contact) {
             return res.status(404).json({ message: 'Contact not found' });
         }
-        res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact
-        });
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(
+            JSON.stringify({
+                status: 200,
+                message: `Successfully found contact with id ${contactId}!`,
+                data: contact
+            }, null, 2)
+        );
     } catch (error) {
-        res.status(500).json({
-            status: 500,
-            message: 'Error fetching contact',
-            error: error.message
-        });
+        res.status(500).send(
+            JSON.stringify({
+                status: 500,
+                message: 'Error fetching contact',
+                error: error.message
+            }, null, 2)
+        );
     }
 };
