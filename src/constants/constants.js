a1170bfc4cdb17681d902780d4ad5e04de1bd7f9
Dennis Fedorov <115027699+DennisUser1@users.noreply.github.com>
2024-11-28 22:41:46 +0200

    v.1.9. Fixed bugs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,7 @@
 export const contactTypeList = ['work', 'home', 'personal'];
 export const booleanList = ['true', 'false'];
 export const sortOrderList = ['asc', 'desc'];
+export const sortByList = ['_id', 'name', 'phoneNumber', 'email', 'isFavourite', 'contactType', 'createdAt'];
 
 export const phoneNumberPattern = /^\+380\d{9}$/;
 export const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -1,10 +1,11 @@
-import { sortOrderList } from '../constants/constants.js';
+import { sortOrderList, sortByList } from '../constants/constants.js';
 
-export function parseSortParams ({ sortBy, sortOrder }, fieldList) {
+export function parseSortParams ({ sortBy, sortOrder }) {
     const parsedSortOrder = sortOrderList.includes(sortOrder)
         ? sortOrder
         : sortOrderList[0];
-    const parsedSortBy = fieldList.includes(sortBy) ? sortBy : fieldList[0];
+    const parsedSortBy = sortByList.includes(sortBy) ? sortBy : 'name';
+
     return {
         sortBy: parsedSortBy,
         sortOrder: parsedSortOrder,
